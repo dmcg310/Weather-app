653eb3bb1c7f5a418179bcdcd5d7f86a0d5eabbe
dmcg310 <120114728+dmcg310@users.noreply.github.com>
2023-03-03 19:01:40 +0000

    feat: button for unit conversion

--- src/api.js
+++ src/api.js
@@ -1,61 +1,70 @@
 import { dom } from "./dom.js";
 
 function getUserSelectedCity() {
-	const container = document.getElementById("weather-information-container");
-	const form = document.getElementById("form");
+  const container = document.getElementById("weather-information-container");
+  const form = document.getElementById("form");
 
-	form.addEventListener("submit", (e) => {
-		e.preventDefault();
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-		container.innerHTML = "";
+    container.innerHTML = "";
 
-		const city = document.getElementById("user-city").value;
-		getWeather(city);
-	});
+    const city = document.getElementById("user-city").value;
+    getWeather(city);
+  });
 }
 
 async function getWeather(city) {
-	const response = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=050bbef77d1e1eeb2549979a3c38ae80&units=metric`,
-		{ mode: "cors" }
-	);
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=050bbef77d1e1eeb2549979a3c38ae80&units=metric`,
+    { mode: "cors" }
+  );
 
-	if (!response.ok) {
-		const modal = document.getElementById("validation-error-modal");
-		modal.style.display = "block";
+  if (!response.ok) {
+    const modal = document.getElementById("validation-error-modal");
+    modal.style.display = "block";
 
-		const closeBtn = document.getElementById("validation-error-modal-close");
-		closeBtn.addEventListener("click", () => {
-			modal.style.display = "none";
-		});
+    const closeBtn = document.getElementById("validation-error-modal-close");
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
 
-		window.addEventListener("click", (e) => {
-			if (e.target == modal) {
-				modal.style.display = "none";
-			}
-		});
+    window.addEventListener("click", (e) => {
+      if (e.target == modal) {
+        modal.style.display = "none";
+      }
+    });
 
-		window.addEventListener("keydown", (e) => {
-			if (e.key === "Escape") {
-				modal.style.display = "none";
-			}
-		});
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        modal.style.display = "none";
+      }
+    });
 
-		return;
-	}
+    return;
+  }
 
-	const data = await response.json();
+  const data = await response.json();
 
-	const weatherMain = data.weather[0].main;
-	const temp = data.main.temp;
-	const feelsLike = data.main.feels_like;
-	const tempMin = data.main.temp_min;
-	const tempMax = data.main.temp_max;
-	const humidity = data.main.humidity;
-	const windSpeed = data.wind.speed;
-	const name = data.name;
+  const weatherMain = data.weather[0].main;
+  const temp = data.main.temp;
+  const feelsLike = data.main.feels_like;
+  const tempMin = data.main.temp_min;
+  const tempMax = data.main.temp_max;
+  const humidity = data.main.humidity;
+  const windSpeed = data.wind.speed;
+  const name = data.name;
 
-	dom(weatherMain, temp, feelsLike, tempMin, tempMax, humidity, windSpeed, name);
+  dom(
+    weatherMain,
+    temp,
+    feelsLike,
+    tempMin,
+    tempMax,
+    humidity,
+    windSpeed,
+    name
+  );
 }
 
 export { getUserSelectedCity, getWeather };
--- src/dom.js
+++ src/dom.js
@@ -8,6 +8,18 @@ function dom(
   windSpeed,
   name
 ) {
+  let result;
+  let impTemp = conversion(temp);
+  let impFeelsLike = conversion(feelsLike);
+  let impTempMin = conversion(tempMin);
+  let impTempMax = conversion(tempMax);
+
+  function conversion(unit) {
+    result = (unit * 9) / 5 + 32;
+    result = Math.round(result * 100) / 100;
+    return (result += " °F");
+  }
+
   temp += " °C";
   feelsLike += " °C";
   tempMin += " °C";
@@ -27,7 +39,35 @@ function dom(
   <p>Wind ${windSpeed}</p>
   <p>Lowest today is ${tempMin} and highest is ${tempMax}</p>`;
 
-  container.appendChild(card);
+  const conversionButtonDiv = document.createElement("div");
+  conversionButtonDiv.classList = "conversion-button-div";
+  conversionButtonDiv.innerHTML = `<button type="submit" id="unit-conversion-button">Convert unit</button>`;
+
+  container.append(card, conversionButtonDiv);
+
+  domUnitConversion(card);
+
+  function domUnitConversion(card) {
+    const imperialButton = document.getElementById("unit-conversion-button");
+    imperialButton.addEventListener("click", () => {
+      card.innerHTML = `<h3 class="card-header">${name}   /   ${impTemp}</h3>
+    <p>Feels like ${impFeelsLike}</p>
+    <p>${weatherMain}</p>
+    <p>${humidity} Humidity</p>
+    <p>Wind ${windSpeed}</p>
+    <p>Lowest today is ${impTempMin} and highest is ${impTempMax}</p>`;
+
+      imperialButton.addEventListener("click", () => {
+        card.innerHTML = `<h3 class="card-header">${name}   /   ${temp}</h3>
+      <p>Feels like ${feelsLike}</p>
+      <p>${weatherMain}</p>
+      <p>${humidity} Humidity</p>
+      <p>Wind ${windSpeed}</p>
+      <p>Lowest today is ${tempMin} and highest is ${tempMax}</p>`;
+        domUnitConversion(card);
+      });
+    });
+  }
 }
 
 export { dom };
